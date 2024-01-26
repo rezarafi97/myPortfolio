7c117cf5f8fe0459ca704283b06102f6d9e0adc9
Rezarafi97 <rezarafi901@gmail.com>
2024-01-26 19:53:41 +0330

    new project has been added

--- src/components/landing/Introduction.jsx
+++ src/components/landing/Introduction.jsx
@@ -20,7 +20,7 @@ const Introduction = () => {
         <li className={listClass}>
           Click here to see{" "}
           <a
-            href="https://mstorage2.jobinjacdn.com/records/files/uploads/documents/394b52db-ee2d-47dc-8dbc-60c9c36e1bde.pdf?requester=352e3132312e3233322e313437&resource=753a32343937353333&from=6170706c69636174696f6e5f666f726d&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20231209%2F%2Fs3%2Faws4_request&X-Amz-Date=20231209T191509Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=2bad8b08d0cabf3942db250a048f47067329f8e172e98fb06920def57c509076"
+            href="https://mstorage2.jobinjacdn.com/records/files/uploads/documents/e2beecf4-acce-458a-ac17-fd5a8e262d3b.pdf?requester=352e3132312e39372e3333&resource=753a32343937353333&from=6170706c69636174696f6e5f666f726d&X-Amz-Content-Sha256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=oetSom924MQvtCRu9N6vvQ4bt8cG9HSpRnxWU756SyWy5NVt6nTpmfA9aV2b%2F20240126%2F%2Fs3%2Faws4_request&X-Amz-Date=20240126T162221Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=c005923380f1b96c17903f2b038735880c354f515427891acf06194ff7a3eec4"
             className={`${spanClass} animate-pulse`}
           >
             My Resume
--- src/data/index.js
+++ src/data/index.js
@@ -10,11 +10,20 @@ export const projects = [
   },
   {
     id: "1",
-    title: "Dashboard",
+    title: "Nextjs Dashboard",
     description:
       "This project is a dashboard web app has been developed by Next js. This web app has authentication and authorization and has been developed minimal with invoices overview and search and mutation features.",
     imageURL: "https://github.com/rezarafi97/myPortfolio/blob/master/src/assets/nextjs-dashboard.png?raw=true",
     demo: "https://nextjs-dashboard-kr03sx9yx-rezarafi97s-projects.vercel.app/",
     source: "https://github.com/rezarafi97/nextjs-dashboard",
+  },
+  {
+    id: "2",
+    title: "Reactjs Dashboard",
+    description:
+      "This project is a dashboard web app has been developed by React js.",
+    imageURL: "https://github.com/rezarafi97/myPortfolio/blob/master/src/assets/nextjs-dashboard.png?raw=true",
+    demo: "https://dashboard-kappa-ten-17.vercel.app/",
+    source: "https://github.com/rezarafi97/dashboard",
   },
 ];
